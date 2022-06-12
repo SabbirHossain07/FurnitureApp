14924d3869ec26ddaf4c115edc5f7f1b1a162ba7
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-06-12 19:33:37 +0600

    Initial Commit

---
+++ Furniture/FurnitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureApp.swift
+//  Furniture
+//
+//  Created by Sopnil Sohan on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
